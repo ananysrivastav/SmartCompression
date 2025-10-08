d79b01965970e32ffc504ea788cedd537f6711da
ananysrivastav <kumaranany7@gmail.com>
2025-10-08 14:15:39 +0530

    Update min_heap.c

--- src/min_heap.c
+++ src/min_heap.c
@@ -1,13 +1,72 @@
-#include <stdlib.h>
+#include "min_heap.h"
+#include "common.h"
 
-typedef struct Node {
-    char ch;
-    int freq;
-    struct Node *left, *right;
-} Node;
+Node* createNode(char ch, int freq) {
+    Node* temp = (Node*)malloc(sizeof(Node));
+    temp->left = temp->right = NULL;
+    temp->ch = ch;
+    temp->freq = freq;
+    return temp;
+}
 
-typedef struct MinHeap {
-    int size;
-    int capacity;
-    Node **array;
-} MinHeap;
+MinHeap* createMinHeap(int capacity) {
+    MinHeap* minHeap = (MinHeap*)malloc(sizeof(MinHeap));
+    minHeap->size = 0;
+    minHeap->capacity = capacity;
+    minHeap->array = (Node**)malloc(minHeap->capacity * sizeof(Node*));
+    return minHeap;
+}
+
+void swapNode(Node** a, Node** b) {
+    Node* t = *a;
+    *a = *b;
+    *b = t;
+}
+
+void heapify(MinHeap *heap, int idx) {
+    int smallest = idx;
+    int left = 2 * idx + 1;
+    int right = 2 * idx + 2;
+
+    if (left < heap->size && heap->array[left]->freq < heap->array[smallest]->freq)
+        smallest = left;
+
+    if (right < heap->size && heap->array[right]->freq < heap->array[smallest]->freq)
+        smallest = right;
+
+    if (smallest != idx) {
+        swapNode(&heap->array[smallest], &heap->array[idx]);
+        heapify(heap, smallest);
+    }
+}
+
+Node* extractMin(MinHeap *heap) {
+    if (heap->size == 0)
+        return NULL;
+
+    Node* temp = heap->array[0];
+    heap->array[0] = heap->array[heap->size - 1];
+    --heap->size;
+    heapify(heap, 0);
+
+    return temp;
+}
+
+void insertHeap(MinHeap *heap, Node *node) {
+    ++heap->size;
+    int i = heap->size - 1;
+
+    while (i && node->freq < heap->array[(i - 1) / 2]->freq) {
+        heap->array[i] = heap->array[(i - 1) / 2];
+        i = (i - 1) / 2;
+    }
+
+    heap->array[i] = node;
+}
+
+void freeMinHeap(MinHeap *heap) {
+    if (heap) {
+        free(heap->array);
+        free(heap);
+    }
+}
