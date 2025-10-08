dae2a01aa442fe5284a6c69564bcfa314d2c92a0
ananysrivastav <kumaranany7@gmail.com>
2025-10-08 13:44:08 +0530

    Update huffman_tree.c

--- src/huffman_tree.c
+++ src/huffman_tree.c
@@ -1,1 +1,19 @@
+Node* buildHuffmanTree(int freq[]) {
+    MinHeap *heap = createMinHeap(MAX_CHARS);
+    for (int i = 0; i < MAX_CHARS; i++) {
+        if (freq[i] > 0)
+            insertHeap(heap, createNode((char)i, freq[i]));
+    }
+
+    while (heap->size > 1) {
+        Node *left = extractMin(heap);
+        Node *right = extractMin(heap);
+        Node *merged = createNode('$', left->freq + right->freq);
+        merged->left = left;
+        merged->right = right;
+        insertHeap(heap, merged);
+    }
+
+    return extractMin(heap);
+}
 
