ff72adf6ed1318585bf3efb62811bb54b744b4f0
ananysrivastav <kumaranany7@gmail.com>
2025-10-08 14:09:17 +0530

    Update huffman_tree.h

--- input/huffman_tree.h
+++ input/huffman_tree.h
@@ -1,9 +1,9 @@
 #ifndef HUFFMAN_TREE_H
 #define HUFFMAN_TREE_H
 
-#include "huffman.h"
+#include "common.h"
 #include "min_heap.h"
 
 Node* buildHuffmanTree(int freq[]);
 
-#endif // HUFFMAN_TREE_H
+#endif
