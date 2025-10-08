3611f0e9c48bde2babf538990252483a8c1328ca
ananysrivastav <kumaranany7@gmail.com>
2025-10-08 14:06:30 +0530

    Update and rename code_generation.h to common.h

--- input/code_generation.h
+++
@@ -1,9 +0,0 @@
-#ifndef CODE_GENERATION_H
-#define CODE_GENERATION_H
-
-#include "huffman.h"
-#include <string.h>
-
-void generateCodes(Node *root, char *code, int depth, char codes[][MAX_CHARS]);
-
-#endif // CODE_GENERATION_H
---
+++ input/common.h
@@ -0,0 +1,22 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_CHARS 256
+
+typedef struct Node {
+    char ch;
+    int freq;
+    struct Node *left, *right;
+} Node;
+
+typedef struct MinHeap {
+    int size;
+    int capacity;
+    Node **array;
+} MinHeap;
+
+#endif
