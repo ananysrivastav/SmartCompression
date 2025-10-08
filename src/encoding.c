9d57ba599cb29e4a34a67d077c577d69938436a3
ananysrivastav <kumaranany7@gmail.com>
2025-10-08 14:17:09 +0530

    Update encoding.c

--- src/encoding.c
+++ src/encoding.c
@@ -1,6 +1,16 @@
+#include "encoding.h"
+#include "common.h"
+
 void encodeFile(const char *input, const char *output, char codes[][MAX_CHARS]) {
     FILE *in = fopen(input, "r");
     FILE *out = fopen(output, "w");
+
+    if (!in || !out) {
+        perror("File open error during encoding");
+        if (in) fclose(in);
+        if (out) fclose(out);
+        return;
+    }
 
     char ch;
     while ((ch = fgetc(in)) != EOF) {
